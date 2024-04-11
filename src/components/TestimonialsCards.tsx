94469a41eb92e82c4ebfbccf58a2cd18e4ca4018
Aavash Paudel <muproject990@gmail.com>
2024-04-11 23:20:53 +0545

    solve

--- src/components/TestimonialsCards.tsx
+++ src/components/TestimonialsCards.tsx
@@ -1,6 +1,5 @@
 'use client'
-
-import { InfiniteMovingCards } from "./ui/infinite-moving-cards"
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 const musicSchoolTestimonials = [
     {
@@ -34,11 +33,12 @@ const musicSchoolTestimonials = [
         title: 'Music Production Student',
     },
 ];
+
 function MusicSchoolTestimonials() {
     return (
-        <div>
+        <div className="h-[40rem] w-full dark:bg-purple  dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
             <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
-            <div className="flex justify-center w-full overflow-hidden px-4 sm:px6 lg:px-8" > 
+            <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
                 <div className="w-full max-w-6xl">
                     <InfiniteMovingCards
                         items={musicSchoolTestimonials}
